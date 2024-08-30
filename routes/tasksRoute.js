71177308efe94f529ac29f7413b124e0ffac4cc1
Đức Hồ <hovuminhduc@gmail.com>
2024-08-30 19:17:26 +0700

    Router, controller in vanilla NodeJS

---
+++ routes/tasksRoute.js
@@ -0,0 +1,9 @@
+const controller = require('../controllers');
+
+module.exports = {
+  GET:    controller.tasks.getTask,
+  POST:   controller.tasks.createTask,
+  PUT:    controller.tasks.updateTask,
+  DELETE: controller.tasks.updateTask,
+};
+
